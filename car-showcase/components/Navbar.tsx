8b597db4fde69d17e6c90013ca3ef41991a0d91c
Aayush10Kanholikar <akanolikar@gmail.com>
2024-06-21 23:43:27 +0530

    initial commit

---
+++ car-showcase/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import { CustomButtom } from ".";
+
+const Navbar = () => {
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link href={"/"} className="flex justify-center items-center">
+          <Image
+            src={"/logo.svg"}
+            alt="Car Hub Logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
+        <CustomButtom
+          btnType="button"
+          title="Sign In"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ car-showcase/components/index.ts
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import CustomButtom from "./CustomButtom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from './CarDetails'
+import ShowMore from './ShowMore'
+
+export { Hero,CarCard, CustomButtom, ShowMore, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer };
